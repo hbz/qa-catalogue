22bc16879f05762a1bf344fd2195e0b2041d9059
Phu Tu <phu.tu@hbz-nrw.de>
2024-11-20 09:21:54 +0100

    Update date of basedump

--- web-classes/catalogue/Hbz.php
+++ web-classes/catalogue/Hbz.php
@@ -2,7 +2,7 @@
 
 class Hbz extends Catalogue {
   protected $name = 'hbz';
-  protected $label = 'hbz Verbunddaten Basedump vom 23.09.2024';
+  protected $label = 'hbz Verbunddaten Basedump vom 04.11.2024';
   protected $url = 'https://lobid.org/';
   protected $marcVersion = 'HBZ';
   protected $linkTemplate = 'https://lobid.org/marcxml/{id}';
