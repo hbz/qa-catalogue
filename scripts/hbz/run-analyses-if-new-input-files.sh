60ed9030715ed1b229c6b2049b519ee574fe7d1c
Phu Tu <phu.tu@hbz-nrw.de>
2024-11-20 09:10:31 +0100

    Add simple email notification

--- scripts/hbz/run-analyses-if-new-input-files.sh
+++ scripts/hbz/run-analyses-if-new-input-files.sh
@@ -9,20 +9,22 @@ TASKS=${2:-all}
 DATADIR="${APPDIR}/input/${CATALOGUE}/marc"
 CHECKFILE="${DATADIR}/check_file.txt"
 RUNNINGFILE="${APPDIR}/running_${CATALOGUE}_analyses.txt"
+MAILRECIPIENT="phu.tu@hbz-nrw.de,tobias.buelte@hbz-nrw.de"
 
 find ${DATADIR} -newer "$CHECKFILE" >/tmp/newfiles$$
 
 if [[ -s "/tmp/newfiles$$" ]] ; then
   echo "files changed: "
   cat "/tmp/newfiles$$"
-  echo "$(date) Now start qa catalogue analyses"
+  echo "$(date) Now start analysis for ${CATALOGUE}" | tee | mail -s "QA catalogue: start analysis ${CATALOGUE}" $MAILRECIPIENT
+  # analysis still running?
   if [[ -f "$RUNNINGFILE" ]]; then
-    echo "$(date) Oops, there are analyses still running! Try again later."
+    echo "$(date) Oops, previous analysis is still running! Try again later." | tee | mail -s "QA catalogue: try again later" $MAILRECIPIENT
   else
     touch "$RUNNINGFILE"
     cd $APPDIR
     docker exec -i metadata-qa-marc ./catalogues/${CATALOGUE}.sh ${TASKS}
-    echo "$(date) Finished analyses for ${CATALOGUE}!"
+    echo "$(date) Finished analysis for ${CATALOGUE}!" | tee | mail -s "QA catalogue: Finished!" $MAILRECIPIENT
     rm "$RUNNINGFILE"
     touch "$CHECKFILE"
     rm "/tmp/newfiles$$"
