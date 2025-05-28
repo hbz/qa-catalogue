55783b7f6826d3ec63ee8825d08882bbb12d4eba
TobiasNx <tobias.buelte@hbz-nrw.de>
2025-05-28 09:16:39 +0200

    Separate lobid publishing tags from other hbz tags #23

---
+++ src/main/java/de/gwdg/metadataqa/marc/definition/tags/lobidtags/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains the tags specifics at Hochschulbibliothekszentrum after publishing from ALMA for lobid complementing the lobidtags
+ *
+ * The source of the classes is this document:
+ *
+ * https://service-wiki.hbz-nrw.de/pages/viewpage.action?pageId=949911658
+ */
+package de.gwdg.metadataqa.marc.definition.tags.lobidtags;
+
+//rest of the file is empty
