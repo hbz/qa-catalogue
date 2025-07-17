b98d754ef004e46eb2f66b429d5f490a87466a54
Phu Tu <phu.tu@hbz-nrw.de>
2025-07-17 15:53:56 +0200

    Add configs for separate catalogue lobid publishing

---
+++ web-classes/catalogue/Hbz_lobid_publishing.php
@@ -0,0 +1,9 @@
+<?php
+
+class Hbz_lobid_publishing extends Catalogue {
+  protected $name = 'hbz_lobid_publishing';
+  protected $label = 'BIBLIOGRAPHIC_125852704360006441_125852704340006441_1.xml.gz vom 15.07.2025';
+  protected $url = 'https://lobid.org/';
+  protected $marcVersion = 'HBZ';
+  protected $linkTemplate = 'https://lobid.org/marcxml/{id}';
+}
